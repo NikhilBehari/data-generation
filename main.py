b2cb38b3bb17b229fac2730b8680054c756eb5da
Nikhil <nikhilbehari@college.harvard.edu>
2020-05-04 20:37:50 -0400

    Cluster data generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import multivariate_normal
 
 def generate_data_linear(low_bound=1, high_bound=10, num_points=50, b1=3.55, b0=0, error_var=6):
     # generate data with linear relationship using Gaussian error
@@ -10,7 +10,33 @@ def generate_data_linear(low_bound=1, high_bound=10, num_points=50, b1=3.55, b0=
     return np.array([x_vals, y_vals])
 
 
-# sample usage
-data = generate_data_linear(0, 1, 50, 3, 0, 1)
-plt.scatter(data[0], data[1])
-plt.show()
+def linear_data_example():
+    # sample usage
+    data = generate_data_linear(0, 1, 50, 3, 0, 1)
+    plt.scatter(data[0], data[1])
+    plt.show()
+
+
+def generate_data_cluster(num_points=100, sample_balance=10, k=2, dims=3):
+    # generate n-D data clusters
+    sample_prob = np.random.dirichlet(np.ones(k)*sample_balance, size=1)[0]
+    means = np.multiply(np.random.rand(k, dims), np.random.randint(1, 4, dims))
+    covariances = np.random.rand(k, dims, dims)
+    x_data = []
+    y_data = []
+    for n in range(num_points):
+        index = np.random.choice(k, 1, p=sample_prob)[0]
+        x_data.append(multivariate_normal.rvs(means[index], covariances[index]))
+        y_data.append(index)
+
+    return [np.array(x_data), np.array(y_data)]
+
+
+def cluster_data_example():
+    # visualize 2D data clusters
+    data = generate_data_cluster(num_points=500, sample_balance=100)
+    x_data = data[0].T
+    plt.scatter(x_data[0], x_data[1], c=data[1])
+    plt.show()
+
+
