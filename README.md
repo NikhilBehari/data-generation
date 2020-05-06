568dc1c88118e636a1658dc8a0c245da38f0730b
Nikhil <nikhilbehari@gmail.com>
2020-05-05 21:18:03 -0400

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # data-generation
-Data generating tool
+This repo contains methods for generating continuous and categorical data.
+
+### Continuous data for linear regressions
+Produces data with a linear relationship 
+
+![Linear Data Example]
+(linear_example.png)
+
+### Categorical cluster data
+Produces k random clusters with Multinomial distribution
+
+![Cluster Data Example]
+(cluster_example.png)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import multivariate_normal
 
+
 def generate_data_linear(low_bound=1, high_bound=10, num_points=50, b1=3.55, b0=0, error_var=6):
     # generate data with linear relationship using Gaussian error
     x_vals = np.random.uniform(low_bound, high_bound, num_points)
@@ -12,15 +13,15 @@ def generate_data_linear(low_bound=1, high_bound=10, num_points=50, b1=3.55, b0=
 
 def linear_data_example():
     # sample usage
-    data = generate_data_linear(0, 1, 50, 3, 0, 1)
+    data = generate_data_linear(0, 1, 150, 3, 0, 1)
     plt.scatter(data[0], data[1])
     plt.show()
 
 
-def generate_data_cluster(num_points=100, sample_balance=10, k=2, dims=3):
+def generate_data_cluster(num_points=100, sample_balance=10, k=2, dims=3, random_factor=10):
     # generate n-D data clusters
     sample_prob = np.random.dirichlet(np.ones(k)*sample_balance, size=1)[0]
-    means = np.multiply(np.random.rand(k, dims), np.random.randint(1, 4, dims))
+    means = np.multiply(np.random.rand(k, dims), np.random.randint(1, random_factor, dims))
     covariances = np.random.rand(k, dims, dims)
     x_data = []
     y_data = []
@@ -34,9 +35,9 @@ def generate_data_cluster(num_points=100, sample_balance=10, k=2, dims=3):
 
 def cluster_data_example():
     # visualize 2D data clusters
-    data = generate_data_cluster(num_points=500, sample_balance=100)
+    data = generate_data_cluster(num_points=500, sample_balance=10, k=5, random_factor=10)
     x_data = data[0].T
-    plt.scatter(x_data[0], x_data[1], c=data[1])
+    plt.scatter(x_data[0], x_data[1], c=data[1], alpha=0.8, edgecolors='none', cmap='Spectral')
     plt.show()
 
-
+cluster_data_example()
